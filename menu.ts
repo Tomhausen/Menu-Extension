bfa81fd4a70c7bd3ac2eeba436508d97fa7d3b74
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-02-24 16:30:35 +0000

    made pausing menu creation separate function

--- menu.ts
+++ menu.ts
@@ -1,38 +1,29 @@
 
 namespace miniMenu{
 
-
-    function createMenuAndPauseGame(items: miniMenu.MenuItem[]): miniMenu.MenuSprite{
-        let img = image.screenImage().clone();
-        game.pushScene();
-        scene.setBackgroundImage(img)
-        let myMenu = miniMenu.createMenuFromArray(items);
-        myMenu.onButtonPressed(controller.A, function (selection: string, selectedIndex: number) {
-            myMenu.close(); 
-            game.popScene();
-        })
-        return myMenu
-    }
-
     /**
-     * Creates and returns a menu from an array of menu items passed in. Can toggle if the game is paused or not
-     */
-    //% blockId=createPauseableMenu
-    //% block="create menu from $items where pause is $isPaused"
+    * Creates and returns a menu from an array of menu items and pauses the game whilst the menu is open
+    */
+    //% blockId=createMenuFromArrayAndPauseGame
+    //% block="create menu from $items and pause game"
     //% blockSetVariable=myMenu
     //% items.shadow=lists_create_with
     //% items.defl=mini_menu_create_menu_item
     //% group="Create"
     //% weight=99
 
-    export function createPauseableMenu(isPaused: boolean, items: miniMenu.MenuItem[]): miniMenu.MenuSprite{
-        let myMenu: miniMenu.MenuSprite;
-        if (isPaused){
-            myMenu = createMenuAndPauseGame(items);
-        }
-        else{
-            myMenu = createMenuFromArray(items);
-        }
+    function createMenuFromArrayAndPauseGame(items: miniMenu.MenuItem[]): miniMenu.MenuSprite{
+        let img = image.screenImage().clone();
+        game.pushScene();
+        scene.setBackgroundImage(img)
+        let myMenu = miniMenu.createMenuFromArray(items);
+        let closed = false;
+        game.onUpdate(() => {
+            if ((!myMenu || !!(myMenu.flags & sprites.Flag.Destroyed)) && !closed){
+                game.popScene();
+                closed = true;
+            }
+        })
         return myMenu
     }
 
